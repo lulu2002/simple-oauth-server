5b63259f4d06df37ada579ed252dbc11a489bb89
lulu2002 <lose9749@gmail.com>
2024-10-18 13:04:49 +0800

    chore: add more vars to env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,12 +22,13 @@ import dotenv from 'dotenv';
 
 async function main() {
   dotenv.config({path: '../.env'});
+  const env = process.env;
 
   const fastifyInstance = fastify();
   fastifyInstance.register(fastifyFormbody);
   fastifyInstance.register(fastifyCookie);
   fastifyInstance.register(fastifyCors, {origin: true});
-  fastifyInstance.register(fastifyJwt, {secret: process.env.JWT_SECRET!});
+  fastifyInstance.register(fastifyJwt, {secret: env.JWT_SECRET!});
 
   const dataSource = new DataSource({
     type: 'sqlite',
@@ -62,14 +63,14 @@ async function main() {
   authController.registerRoutes(fastifyInstance);
 
   await authRepo.create({
+    id: env.PREDEFINE_CLIENT_ID!,
+    name: env.PREDEFINE_CLIENT_NAME!,
+    redirectUris: [env.PREDEFINE_CLIENT_REDIRECT_URI!],
+    secret: await passwordHashing.hash(env.PREDEFINE_CLIENT_SECRET!),
     allowOrigins: [],
-    id: "test_client",
-    name: "test client",
-    redirectUris: ["http://localhost:5173/callback"],
-    secret: await passwordHashing.hash("test_secret")
   });
 
-  fastifyInstance.listen({port: 8080}, (err, address) => {
+  fastifyInstance.listen({port: Number(env.PORT!)}, (err, address) => {
     if (err) {
       console.error(err)
       process.exit(1)
